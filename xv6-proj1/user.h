c33283640955d3e70e03c0b685108ea734d08ae9
Zhuoyi04 <joeyvinji04@gmail.com>
2025-09-27 01:29:37 +0900

    PJ1: add syscall interfaces for setnice/getnice/ps (numbers + user stubs)

--- xv6-proj1/user.h
+++ xv6-proj1/user.h
@@ -23,6 +23,9 @@ int getpid(void);
 char* sbrk(int);
 int sleep(int);
 int uptime(void);
+int getnice(int pid);
+int setnice(int pid, int nice);
+int ps(void);
 
 // ulib.c
 int stat(const char*, struct stat*);
